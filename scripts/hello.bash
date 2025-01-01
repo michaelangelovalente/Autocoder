e57b3e9d570c35d05262e7ad6d548643cb1bcde4
michaelangelovalente <miky10.mv@gmail.com>
2025-01-01 14:49:02 +0100

    No significant commit message

--- scripts/hello.bash
+++ scripts/hello.bash
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
 
-GITHUB_TOKEN="$1"
-REPOSITORY="$2"
-ISSUE_NUMBER="$3"
-OPENAI_API_KEY="$4"
+# GITHUB_TOKEN="$1"
+# REPOSITORY="$2"
+# ISSUE_NUMBER="$3"
+# OPENAI_API_KEY="$4"
+
+
+GITHUB_TOKEN_1=${GITHUB_TOKEN}
+REPOSITORY=${REPO_NAME}
+ISSUE_NUMBER=${ISSUE_NUMBER}
+OPENAI_API_KEY=${OPENAI_API_KEY}
 
 echo "HELLO TEST";
-echo "Using token: $GITHUB_TOKEN"
+echo "Using token: $GITHUB_TOKEN_1"
 echo "Using OpenAI key: $OPENAI_API_KEY"
 echo "Issue number: $ISSUE_NUMBER"
 echo "Repository: $REPOSITORY"
